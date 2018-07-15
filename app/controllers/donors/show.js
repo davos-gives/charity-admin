aa7392da292eccb3d04b2f10c417ac020d68ee6a
ian knauer <ian.knauer@gmail.com>
2018-07-15 11:35:32 -0700

    added most recent donation

--- app/controllers/donors/show.js
+++ app/controllers/donors/show.js
@@ -4,9 +4,26 @@ import { computed } from '@ember/object';
 
 export default Controller.extend({
 
-  donationAmounts: computed('model.donations.@each', function() {
-    return this.get('model.donations').mapBy('amount');
+  totalDonations: computed('model.donations.@each.amount', function() {
+    return this.get('model.donations').mapBy('amount').reduce((a,b) => a + b, 0)
   }),
 
-  donationsTotal: computed.sum('donationAmounts'),
+  yearlyDonations: computed('model.donations.@each.frequency', function() {
+    let date = new Date();
+    date.setHours(0,0,0,0);
+    return this.get('model.donations').filter(donation => donation.get('timestamp') > date);
+  }),
+
+  yearlyDonationAmount: computed('yearlyDonations', function() {
+    return this.get('yearlyDonations').mapBy('amount').reduce((a,b) => a + b, 0)
+  }),
+
+  giftsSortingDesc: Object.freeze(['timestamp:desc']),
+
+  orderedGifts: computed.sort('yearlyDonations', 'giftsSortingDesc'),
+
+  firstOrderedGift: computed('orderedGifts', function() {
+    return this.get('orderedGifts')[0];
+  }),
+
 });
