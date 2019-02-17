265e38173c32e4b20f143e8bdebfebbbb3bd2934
ian knauer <ian.knauer@gmail.com>
2019-02-16 16:36:34 -0800

    update for https

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,5 +4,5 @@ import { inject as service } from "@ember/service";
 
 export default DS.JSONAPIAdapter.extend({
   namespace: "api/admin",
-  host: 'http://app.davos.gives',
+  host: 'https://app.davos.gives',
 })
